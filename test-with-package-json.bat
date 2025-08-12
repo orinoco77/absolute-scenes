cda50e8ae524ea78436acb96e80f3c0a25e4fedf
Adam Short <ajs@shiny.org.uk>
2025-08-12 17:54:29 +0100

    remove batch files

--- test-with-package-json.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo === Testing with package.json password ===
-
-echo The package.json now has certificatePassword: "scenes"
-echo Testing npm run dist (the original working command)...
-
-npm run dist
-
-pause
